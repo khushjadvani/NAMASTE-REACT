21db6e361dc69f594f5a19ffed58e8270ea872ab
Khush <jadvanikhush32@gmail.com>
2024-02-05 18:05:05 +0530

    Your commit message

--- 1App.js
+++ 1App.js
@@ -1,3 +1,8 @@
+//after installing raect in our code after that we import our file 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 // const heading = React.createElement("h1", {}, "Hello World From React JS");
 // //root at uper side so we declare in react to know them
 // const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -6,23 +11,63 @@
 
 //after giving parameter object
 // const heading = React.createElement("h1", {id:"heading", XYZ :"abc"}, "Hello World From React JS");
-//root at uper side so we declare in react to know them
+
+//real function component
+
+
+// const parent = React.createElement(
+//     "div", 
+//     { id: "parent" },
+//         React.createElement(
+//             "div", 
+//             { id: "child" }, 
+//                  [React.createElement("h1", {}, "I'm in h1 tag again"),
+//                  React.createElement("h1", {}, "I'm an h1 tag")]
+
+//                         )       );
 // const root = ReactDOM.createRoot(document.getElementById("root"));
-//append that
-// root.render(heading);
+// root.render(parent);
+// console.log(parent);
 
-const parent = React.createElement(
-    "div", 
-    { id: "parent" },
-        React.createElement(
-            "div", 
-            { id: "child" }, 
-                 [React.createElement("h1", {}, "I'm an h1 tag"),
-                 React.createElement("h1", {}, "I'm an h1 tag")]
+  // by using jsx and create heading tag
+// const JSXHeading = <h1 id ="heading">NAMASTE REACT using JSX</h1>;
 
-                        )       );
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-console.log(parent);
+// root at uper side so we declare in react to know them
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// append that
+// root.render(JSXHeading);
 
-  
+//by using functional component
+
+    //react element
+    // const f1 = (
+    //     <h1 className  = "head">
+    //         namste react using JSX
+    //     </h1>
+    // );
+
+    // react function component
+    // const Headingf1 = () => {
+    //     return <h1>namaste react using function componrent</h1>
+    // };
+    // const Headingf2 = () => (
+    //   <h1>namaste react using function componrent</h1>
+    //   );
+    //   const root = ReactDOM.createRoot(document.getElementById("root"));
+    //   root.render(<Headingf2/>);
+    
+    //component composition
+    const Title = () => (
+      <h1> namste react in title</h1>
+    );
+    const HeadingComponent =  () =>
+    (
+      <div>
+        <Title/>
+        <h1> namste react in HeadingCOmponent</h1>
+      </div>
+    );
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(<HeadingComponent/>)
+    
+
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 #all React Source Code
+
+# parcel is 
+
+  -for more detail go throw parcel website 
+    - dev build 
+    - local server
+    - HMR  = hot module replacement
+    - file Watching algorithm - Written in c++
+    - caching - faster builds
+    - image optimization 
+    - bundling
+    - compressing
+    - consistance hashing
+    - code spliting for you 
+    - differencial bundeling - support older browser
+    - dignostic  // give error beatifully
+    - good error handling 
+    - tree shaking      // remove unused code for u
